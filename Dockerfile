52c7bf9b88b2dd26624efe6a8e1d20dc1e7c71de
Ethan Lipkind <Ethan.lipkind@gmail.com>
2018-04-18 12:55:17 -0400

    reading data successfully

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,5 @@
-# To be filled in
+FROM node:8-alpine
+WORKDIR /foo
+COPY . .
+RUN npm install
+CMD ["node", "checkSum.js"]
--- checkSum.js
+++ checkSum.js
@@ -17,10 +17,91 @@
 	Your checksum would be 3004.
 */
 
+const redis = require("redis");
+const bluebird = require('bluebird');
+const client = redis.createClient("http://redis");
+client.on("connect", () => console.log("connected successfully to redis!"));
+client.on("error", err => console.log("redis connection error: ", err));
+
+bluebird.promisifyAll(client); // allows for chaining of .then and .catch
+
+client.keysAsync('*')
+.then(keys => {
+	let sum = 0;
+	keys.forEach(key => {
+		client.typeAsync(key).then(type => {
+			if ( type === 'list' ) {
+				client.llenAsync(key).then(l => {
+					client.lrangeAsync(key, 0, l).then(arr => {
+						data.push(arr);
+						if ( data.length === keys.length ) {
+							console.log('donezo!', data);
+						} else {
+							console.log('not yet');
+						}
+					}).catch(e => console.log('lrange error: ', e));
+				}).catch(e => console.log('llen error', e));
+			} else if ( type === 'set' ) {
+				client.smembersAsync(key).then(arr => {
+					data.push(arr);
+					if ( data.length === keys.length ) {
+						console.log('donezo!!', data);
+					} else {
+						console.log('not yet');
+					}
+				}).catch(e => console.log('smembers error: ', e));
+			}
+		}).catch(e => {
+			console.log('error getting type of key: ', e);
+		})
+	})
+}).catch(e => console.log(e));
+
+// client.keys('*', (err, replies) => {
+// 	if ( err ) {
+// 		console.log('client keys error: ', err);
+// 	} else {
+// 		let data = [];
+// 		replies.forEach(key => {
+// 			client.type(key, (err, res) => {
+// 				if ( err ) throw err;
+// 				console.log('type of key', key, ': ', res);
+// 				if ( res === "set" ) {
+// 					console.log('do something with the set')
+// 					data.push(res);
+// 					if ( data.length === 500 ) {
+// 						console.log('all done! time to call checkSum on the data.');
+// 					} else {
+// 						console.log('not yet!');
+// 					}
+// 				} else if ( res === "list" ) {
+// 					client.llen(key, (err, length) => {
+
+// 					});
+// 					// let a = client.lrange(key, 0, len);
+// 					console.log('key: ', key, 'len: ', len);
+// 					data.push(res);
+// 					if ( data.length === 500 ) {
+// 						console.log('all done! time to call checkSum on the data.');
+// 					} else {
+// 						console.log('not yet!');
+// 					}
+// 				}
+// 			});
+// 		})
+// 	}
+// });
+
+/*
+* @param {array} row - an array of strings representing numbers in a row of data
+* @return {boolean} - whether the row contains any numerical anagrams
+*/
+
 let containsAnagrams = row => {
 	// copy the row to avoid mutating the input
 	let copy = row.slice();
 	for (var i = 0; i < copy.length; i++) {
+		// sort the digits inside each number so anagrams will appear as the same number
 		let key = copy[i]
 			.split("")
 			.sort((a, b) => parseInt(a) - parseInt(b))
@@ -28,7 +109,9 @@ let containsAnagrams = row => {
 		copy[i] = parseInt(key);
 	}
 
-	copy = copy.sort((a, b) => a - b);
+	// sort the entire array so any anagrams will be adjacent
+	// and therefore identifiable in a single pass over the array
+	copy.sort((a, b) => a - b);
 
 	let candidate = copy[0];
 	for (i = 1; i < copy.length; i++) {
@@ -41,8 +124,12 @@ let containsAnagrams = row => {
 	return false;
 };
 
+/*
+* @param {array} row - a sorted array of numbers
+* @return {boolean} - whether any two elements in the array can be divided to equal 177
+*/
 let divCheck = row => {
-	// since the row is sorted, we can use two pointers to check for the condition:
+	// since the row is sorted, we can use two pointers to scan the array outside-in:
 	let a = 0,
 		b = row.length - 1,
 		flag = false;
@@ -58,47 +145,49 @@ let divCheck = row => {
 	return flag;
 };
 
-let checkSum = data => {
+/*
+* @param {array} row - a single row from the redis database
+* @return {Number} sum - the checkSum calculated after processing every row
+*/
+let checkSum = row => {
 	let sum = 0;
-	data.forEach(row => {
-		// sort the row before calling helper functions
-		let sortedRow = row.slice();
-		sortedRow.sort((a, b) => parseInt(a) - parseInt(b));
+	// data.forEach(row => {
+		// sort the row before calling divCheck
+		let sortedRow = row.slice().sort((a, b) => parseInt(a) - parseInt(b));
 		if (!containsAnagrams(row) && !divCheck(sortedRow)) {
-			// since row is sorted:
+			// min/max will be first and last elements in sortedRow
 			sum += sortedRow[sortedRow.length - 1] - sortedRow[0];
 		}
-	});
-
+	// });
 	return sum;
 };
 
-let assert = (condition, description) => {
-	if (condition) {
-		console.log("test passed.");
-	} else {
-		console.log("test failed: ", description);
-	}
-};
+// let assert = (condition, description) => {
+// 	if (condition) {
+// 		console.log("test passed!!!");
+// 	} else {
+// 		console.log("test failed: ", description);
+// 	}
+// };
 
-let testData = [
-	["1", "2", "3", "4", "5"],
-	["100", "150", "215", "80", "152"],
-	["500", "354", "50", "2", "99"],
-	["3001", "4", "1", "9", "500"]
-];
+// let testData = [
+// 	["1", "2", "3", "4", "5"],
+// 	["100", "150", "215", "80", "152"],
+// 	["500", "354", "50", "2", "99"],
+// 	["3001", "4", "1", "9", "500"]
+// ];
 
-assert(
-	containsAnagrams(testData[1]) === true,
-	"it should identify numerical anagrams"
-);
-assert(
-	containsAnagrams(testData[0]) === false,
-	"it should return false when there are no anagrams."
-);
-assert(
-	divCheck(testData[2].slice().sort((a, b) => parseInt(a) - parseInt(b))) ===
-		true,
-	"it should identify numbers that divide to 177"
-);
-assert(checkSum(testData) === 3004, "it should calculate the sum correctly");
+// assert(
+// 	containsAnagrams(testData[1]) === true,
+// 	"it should identify numerical anagrams"
+// );
+// assert(
+// 	containsAnagrams(testData[0]) === false,
+// 	"it should return false when there are no anagrams."
+// );
+// assert(
+// 	divCheck(testData[2].slice().sort((a, b) => parseInt(a) - parseInt(b))) ===
+// 		true,
+// 	"it should identify numbers that divide to 177"
+// );
+// assert(checkSum(testData) === 3004, "it should calculate the sum correctly");
